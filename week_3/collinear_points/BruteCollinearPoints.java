aa332690468b67a2bb716eae8f8c271c50146612
Christopher Marisco <christopher.marisco@gmail.com>
2021-07-31 18:54:03 -0600

    create collinear brute force java file

---
+++ week_3/collinear_points/BruteCollinearPoints.java
@@ -0,0 +1,59 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+/*************************************************************************
+ *  Compilation:  javac BruteCollinearPoints.java
+ *  Execution:    java BruteCollinearPoints
+ *  Dependencies: Point.java, LineSegment.java
+ *
+ *  Brute Force Algorithm meant to solve the Collinear Points problem.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ * Author: Christopher Marisco
+ *************************************************************************/
+public class BruteCollinearPoints {
+
+    private LineSegment[] mySegments;
+
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    public int numberOfSegments() {
+        return mySegments.length;
+    }
+
+    public LineSegment[] segments() {
+        return mySegments;
+    }
+
+    public static void main(String[] args) {
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
+}
