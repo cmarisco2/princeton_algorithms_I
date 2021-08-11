32eca9eeca89f0e20cde602f46687b58bcbfdcd0
Christopher Marisco <christopher.marisco@gmail.com>
2021-08-11 14:31:26 -0600

    remove commented code pertainint to Slope Stack

--- week_3/collinear_points/BruteCollinearPoints.java
+++ week_3/collinear_points/BruteCollinearPoints.java
@@ -51,17 +51,14 @@ public class BruteCollinearPoints {
     private void findSegment(Point[] points) {
         Point max, min;
         double tempSlope, testSlope = Double.NEGATIVE_INFINITY, innerSlope, epsilon = .0001;
-//        Stack<Double> slopes = new Stack<>();
         Stack<Point> stack = new Stack<>();
         for (int i = 0; i < points.length; i++) {
             stack.push(points[i]);
             for (int j = i + 1; j < points.length; j++) {
                 if (j == i) continue;
-//                testSlope = points[i].slopeTo(points[j]);
                 tempSlope = points[i].slopeTo(points[j]);
                 if (testSlope == tempSlope) break;
                 testSlope = tempSlope;
-//                slopes.push(testSlope);
                 stack.push(points[j]);
                 for (int k = j + 1; k < points.length; k++) {
                     if (k == i || k == j) continue;
@@ -84,8 +81,7 @@ public class BruteCollinearPoints {
                     while (!stack.empty())
                         stack.pop();
                 }
-//                while (!slopes.empty())
-//                    slopes.pop();
+
                 testSlope = Double.NEGATIVE_INFINITY;
             }
             while (stack.size() > 1)
